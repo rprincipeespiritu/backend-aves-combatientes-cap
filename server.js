8c9361d19ddf14d395f95ec6d5c00c89e8376ae6
rprincipe <ruben.principe@seidor.com>
2026-03-10 16:02:33 -0500

    parse DATABASE_URL in server.js

--- server.js
+++ server.js
@@ -3,6 +3,16 @@ require('dotenv').config();
 const cds = require('@sap/cds');
 const cors = require('cors');
 
+// Resolver DATABASE_URL de Railway para CAP
+if (process.env.DATABASE_URL) {
+    const url = new URL(process.env.DATABASE_URL);
+    process.env.DB_HOST = url.hostname;
+    process.env.DB_PORT = url.port;
+    process.env.DB_USER = url.username;
+    process.env.DB_PASSWORD = url.password;
+    process.env.DB_NAME = url.pathname.replace('/', '');
+}
+
 const corsOptions = {
     origin: process.env.CORS_ORIGIN || 'http://localhost:8080',
     allowedHeaders: ['Content-Type', 'Authorization'],
